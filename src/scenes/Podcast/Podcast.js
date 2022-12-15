b3d5a316adc0abb44371fc8e02601793fc6fff8f
bogdan.draghia <bgd.draghia@gmail.com>
2022-12-15 20:03:31 +0000

    feat: Podcast details structure

--- src/scenes/Podcast/Podcast.js
+++ src/scenes/Podcast/Podcast.js
@@ -1,5 +1,34 @@
 const Podcast = () => {
-  return <div>Podcast</div>;
+  return (
+  <div >
+    <div>
+      <div>
+        <div>
+          Image
+        </div>
+      </div>
+      <div>
+        <h3>
+          Title
+        </h3>
+        <p>
+          Author
+        </p>
+      </div>
+      <div>
+        <h3>
+          Descriptions:
+        </h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+        </p>
+      </div>
+    </div>
+    <div>
+      Right - Table
+    </div>
+  </div>
+  )
 };
 
 export default Podcast;
